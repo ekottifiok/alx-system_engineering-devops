33368f216ebf70fc3f6e98de4ab7658f0b16fec0
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-13 19:49:13 +0100

    done with task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # create a file in a folder with some requirements
 
 file {'/tmp/school':
-    ensure => 'present',
-    owner => 'www-data',
-    group => 'www-data',
-    mode => '0744',
+    ensure  => 'present',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
     content => 'I love Puppet',
 }
