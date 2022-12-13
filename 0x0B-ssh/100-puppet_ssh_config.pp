c509393fb5e97b82582bdd074311718aa48fd7af
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-13 20:49:37 +0100

    attempting task 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,7 @@
 # connect to a server without typing a password.
 
-file {'~/.ssh/config':
-  ensure    => 'present',
-  mode      => '0664'
-  content   => '57022-web-01
-    HostName 	3.85.175.19
-    User ubuntu
-    IdentityFile ~/.ssh/school
-    IdentitiesOnly yes'
-}
+exec {'/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/sshd_config':}
+
+exec {'/usr/bin/echo "    ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config':}
+
+exec {'/usr/bin/echo "    UsePAM no" >> /etc/ssh/sshd_config':}
