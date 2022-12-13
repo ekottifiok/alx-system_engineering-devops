686564cc9c8979a1c0c40d50f46328bfb844fdbd
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-13 19:46:15 +0100

    edited task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # create a file in a folder with some requirements
 
-file {'/tmp/school'
-    ensure => 'file',
+file {'/tmp/school':
+    ensure => 'present',
     owner => 'www-data',
     group => 'www-data',
     mode => '0744',
