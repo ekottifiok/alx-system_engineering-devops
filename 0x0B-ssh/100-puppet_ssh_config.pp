2f371b552d644e54505b25df79a8b1700173ac13
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-16 18:31:06 +0100

    edited the 100 file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -10,6 +10,4 @@ file_line {'Declare identity file path':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
-  match  => 'IdentityFile ~/.ssh/id_rsa'
-  match_for_absence => true,
 }
