296316e7f9c4b70e7a8d264cdfd6e5139656ef13
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-16 18:00:29 +0100

    attempting to finish 0x0B

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 # connect to a server without typing a password.
 
-exec {'/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/sshd_config':}
+exec {'/usr/bin/echo "    PasswordAuthentication no" >> /etc/ssh/ssh_config':}
 
-exec {'/usr/bin/echo "    ChallengeResponseAuthentication no" >> /etc/ssh/sshd_config':}
+exec {'/usr/bin/echo "    ChallengeResponseAuthentication no" >> /etc/ssh/ssh_config':}
 
-exec {'/usr/bin/echo "    UsePAM no" >> /etc/ssh/sshd_config':}
+exec {'/usr/bin/echo "    UsePAM no" >> /etc/ssh/ssh_config':}
 
 file {'~/.ssh/config':
   ensure    => 'present',
   mode      => '0664'
   content   => '57022-web-01
-    HostName 	3.85.175.19
+    HostName 	100.25.45.223
     User ubuntu
     IdentityFile ~/.ssh/school
     IdentitiesOnly yes'
