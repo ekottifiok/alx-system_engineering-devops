d5c9e8043d4332b27ac89baaa8fa358c03516300
Ifiok Ekott <ekottifiok@gmail.com>
2022-12-21 08:35:33 +0100

    updated task 1

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,28 +1,28 @@
 #install and configure an Nginx server using Puppet manifest
 package { 'nginx':
-ensure => installed,
+  ensure => installed,
 }
 
 file { '/var/www/html/index.html':
-content => 'Hello World!',
+  content => 'Hello World!',
 }
 
 file { '/etc/nginx/html/index.html':
-content => 'Hello World!',
+  content => 'Hello World!',
 }
 
 file { '/usr/share/nginx/html/index.html':
-content => 'Hello World!',
+  content => 'Hello World!',
 }
 
 file_line { 'Add redirection, 301':
-ensure => 'present',
-path   => '/etc/nginx/sites-available/default',
-after  => 'listen 80 default_server', 
-line   => 'rewrite ^/redirect_me http://www.staggeringbeauty.com/ permanent;',
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server', 
+  line   => 'rewrite ^/redirect_me http://www.staggeringbeauty.com/ permanent;',
 }
 
 service { 'nginx':
-ensure  => running,
-require => Package['nginx'],
+  ensure  => running,
+  require => Package['nginx'],
 }
