d01716c48d612b9f860243bb685cb21b960982fa
Ifiok Ekott <ekottifiok@gmail.com>
2023-01-10 23:40:32 +0100

    added permission for select to create_replica_user

--- 0x14-mysql/create_replica_user.sql
+++ 0x14-mysql/create_replica_user.sql
@@ -2,4 +2,5 @@
 -- with necessary permissions
 CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'replica_user280hbtn';
 GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
